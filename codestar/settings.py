b54cdcedcc5f54184fe561aba81d88db84c0c14a
DFCMK <dkalnoky@gmail.com>
2024-02-12 17:12:53 +0000

    Added Comment class model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -93,6 +93,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
+    "127.0.0.1"
     "https://*.herokuapp.com"
 ]
 
