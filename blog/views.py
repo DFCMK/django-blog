50b6bec2b47f669bb4362946d12fc288ae24af14
DFCMK <dkalnoky@gmail.com>
2024-02-16 17:09:08 +0000

    Added back the PostList class in views.py

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,14 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-        """
+    
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
     Display an individual :model:`blog.Post`.
 
     **Context**
@@ -16,8 +23,9 @@ class PostList(generic.ListView):
 
     :template:`blog/post_detail.html`
     """
+
     queryset = Post.objects.filter(status=1)
-    post = get_object_or404(queryset, slug=slug)
+    post = get_object_or_404(queryset, slug=slug)
 
     return render(
         request,
